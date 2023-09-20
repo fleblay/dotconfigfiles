28423d2b04f3c85eb470f50a9be706a28e9107ea
fred <fred@freds-MacBook-Pro.local>
2023-09-20 18:15:16 +0200

    wip install plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,35 @@
+vim.cmd("source ~/.vimrc")
+
 --1. install nvim-lspconfig
+--git clone https://github.com/neovim/nvim-lspconfig.git
 --2. install lsp servers using brew preferably
 require'lspconfig'.tsserver.setup{}
+--brew install vscode-langservers-extracted
 require'lspconfig'.eslint.setup{}
 
-vim.cmd("source ~/.vimrc")
+--3. install nvim-tree
+--git clone https://github.com/nvim-tree/nvim-tree.lua.git
+--git clone https://github.com/nvim-tree/nvim-web-devicons.git
+--install font from https://www.nerdfonts.com/font-downloads
+--IBMPlexMono
+--set the terminal to use it
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup()
+require("nvim-web-devicons").setup()
+
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+--4. install tree-sitter
+--https://github.com/nvim-treesitter/nvim-treesitter.git
+--Install languages (check with :TSInstallInfo
+--:TSInstall typescript
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -30,6 +56,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('i', '<C-l>', vim.lsp.buf.signature_help, opts)
+	--workspace
     vim.keymap.set('n', 'sfa', vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set('n', 'sfr', vim.lsp.buf.remove_workspace_folder, opts)
 	vim.keymap.set('n', 'sfl', function()

3859aec27ec5840c6da1f4971c4385ba89dc2649
Frederic Le Blay <frederic.leblay@shadow.tech>
2023-10-02 18:12:11 +0200

    add tokyonight, lua parser. Next : add eslint shortcut

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,37 @@ vim.cmd("source ~/.vimrc")
 require'lspconfig'.tsserver.setup{}
 --brew install vscode-langservers-extracted
 require'lspconfig'.eslint.setup{}
+--brew install lua-language-server
+require'lspconfig'.lua_ls.setup{
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
 
 --3. install nvim-tree
 --git clone https://github.com/nvim-tree/nvim-tree.lua.git
@@ -26,17 +57,46 @@ vim.opt.termguicolors = true
 
 vim.keymap.set('n', '<leader>to', ':NvimTreeToggle<CR>')
 vim.keymap.set('n', '<leader>tf', ':NvimTreeFindFile<CR>')
-vim.keymap.set('n', '<leader>tc', ':NvimTreeCollapse<CR>')
 
 --USEFULL :
 --P Parent directory
 --< Previous sibling
 --> Next sibling
+--C-] CD
+--o open
 
 --4. install tree-sitter
 --https://github.com/nvim-treesitter/nvim-treesitter.git
 --Install languages (check with :TSInstallInfo
 --:TSInstall typescript
+
+require'nvim-treesitter.configs'.setup{
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+   -- List of parsers to ignore installing (or "all")
+  ignore_install = { "javascript" },
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+--5. install theme with nvim-treesitter support
+--https://github.com/folke/tokyonight.nvim.git
+
+require("tokyonight").setup({
+ style = "storm"
+})
+
+vim.cmd[[colorscheme tokyonight]]
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -63,6 +123,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts) --USEFULL
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('i', '<C-l>', vim.lsp.buf.signature_help, opts) --USEFULL
+    vim.keymap.set('n', 'gl', vim.lsp.buf.signature_help, opts) --USEFULL
 	--workspace
     vim.keymap.set('n', 'sfa', vim.lsp.buf.add_workspace_folder, opts)
     vim.keymap.set('n', 'sfr', vim.lsp.buf.remove_workspace_folder, opts)

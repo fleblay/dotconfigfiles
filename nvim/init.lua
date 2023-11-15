a72770b13d2e13485c7656643b70f19bd4f8facc
Frederic Le Blay <frederic.leblay@shadow.tech>
2023-11-15 18:35:55 +0100

    [CMP] Add support for cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,10 +108,18 @@ require("lazy").setup(
     {
       "neovim/nvim-lspconfig",
       config = function()
+        --brew install typescript-language-server
         require 'lspconfig'.tsserver.setup {}
 
         --brew install vscode-langservers-extracted
-        require 'lspconfig'.eslint.setup {}
+        require 'lspconfig'.eslint.setup({
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+        })
 
         --brew install lua-language-server
         require 'lspconfig'.lua_ls.setup {
@@ -143,6 +151,10 @@ require("lazy").setup(
             return true
           end
         }
+
+        --brew install gopls
+        require 'lspconfig'.gopls.setup {}
+
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
         vim.keymap.set('n', 'sso', vim.diagnostic.open_float)
@@ -226,7 +238,7 @@ require("lazy").setup(
               sourceMaps = true,
               continueOnAttach = true,
               sourceMapPathOverrides = {
-                "./*" ,"${workspaceRoot}"
+                "./*", "${workspaceRoot}"
               }
             },
             {
@@ -287,6 +299,88 @@ require("lazy").setup(
 
         vim.keymap.set('n', '<leader>du', require 'dapui'.toggle)
       end
+    },
+    {
+      'hrsh7th/nvim-cmp',
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip'
+      },
+      config = function()
+        local cmp = require 'cmp'
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              require('luasnip').lsp_expand(args.body)
+            end,
+          },
+          window = {
+            -- completion = cmp.config.window.bordered(),
+            -- documentation = cmp.config.window.bordered(),
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          }),
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' }, -- For luasnip users.
+          }, {
+            { name = 'buffer' },
+          })
+        })
+
+        -- Set configuration for specific filetype.
+        cmp.setup.filetype('gitcommit', {
+          sources = cmp.config.sources({
+            { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+          }, {
+            { name = 'buffer' },
+          })
+        })
+
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = 'buffer' }
+          }
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = 'path' }
+          }, {
+            { name = 'cmdline' }
+          })
+        })
+
+        -- Set up lspconfig.
+        -- FIXME -> Use a loop
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        require('lspconfig')['tsserver'].setup {
+          capabilities = capabilities
+        }
+        require('lspconfig')['gopls'].setup {
+          capabilities = capabilities
+        }
+        require('lspconfig')['lua_ls'].setup {
+          capabilities = capabilities
+        }
+        require('lspconfig')['eslint'].setup {
+          capabilities = capabilities
+        }
+      end,
     },
   }
 )
